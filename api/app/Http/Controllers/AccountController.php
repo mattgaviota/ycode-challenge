2e59701dcf3afba8912f48d54a0adf571ec90b64
Matias Novoa <matias.novoa.gundel@gmail.com>
2020-04-16 21:58:27 -0400

    Added validation and logic for transactions

--- api/app/Http/Controllers/AccountController.php
+++ api/app/Http/Controllers/AccountController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Account;
 use App\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class AccountController extends Controller
 {
@@ -38,21 +40,36 @@ class AccountController extends Controller
      */
     public function makeTransaction(Request $request, Account $account)
     {
-        try {
-            $toAccount = Account::findOrFail($request->to);
-            $toAccount->balance += $request->amount;
-            $account->balance -= $request->amount;
-            $toAccount->save();
-            $account->save();
-            $transaction = new Transaction();
-            $transaction->from = $account->id;
-            $transaction->to = $toAccount->id;
-            $transaction->details = $request->details;
-            $transaction->amount = $request->amount;
-            $transaction->save();
-            return response()->json('Success');
-        } catch (\Exception $e) {
-            dd($e);
+        $validator = Validator::make($request->all(), [
+            'to' => 'required|exists:accounts,id',
+            'amount' => 'required|numeric|min:1',
+            'details' => 'sometimes|string',
+        ],
+        [
+            'to.exists' => 'The destination account does not exists'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        if ($account->balance - $request->amount >= 0) {
+            try {
+                $toAccount = Account::findOrFail($request->to);
+                $toAccount->balance += $request->amount;
+                $account->balance -= $request->amount;
+                $toAccount->save();
+                $account->save();
+                $transaction = new Transaction();
+                $transaction->from = $account->id;
+                $transaction->to = $toAccount->id;
+                $transaction->details = $request->details;
+                $transaction->amount = $request->amount;
+                $transaction->save();
+                return response()->json('Success');
+            } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+                return response()->json('The destination account does not exist', 409);
+            }
+        } else {
+            return response()->json('The amount to transfer exceeds your balance', 422);
         }
 
     }
